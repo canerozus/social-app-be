af97773d0fbd8e1b8a1d1bb070d9fb449fde1948
deepforce08 <caner.ozus@inverizo.com>
2022-12-12 04:00:21 +0300

    middlewares

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser")
 
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
