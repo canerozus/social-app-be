ca83f9cd4e906a29d2d122abe8b8de756a4ef7c9
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-03-20 14:37:09 +0300

    add post

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,6 @@
 const { db } = require("../routes/connect.js");
 const jwt = require("jsonwebtoken");
+const moment = require("moment");
 
 
 const getPosts = (req, res) => {
@@ -8,16 +9,37 @@ const getPosts = (req, res) => {
 
     jwt.verify(token, "secretkey", (err, userInfo) => {
         if (err) return res.status(403).json("Token is not valid!")
-        
+
         const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS U ON (u.id = p.userId)
         LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
         ORDER BY p.createdAt DESC`;
-        
-        db.query(q, [userInfo.id,userInfo.id ], (err, data) => {
+
+        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
             if (err) return res.status(500).json(err)
             return res.status(200).json(data)
         })
     })
 
 }
-module.exports = { getPosts }
+const addPost = (req, res) => {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json("Not Logged In!")
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!")
+
+        const q = "INSERT INTO posts (`desc`,`img`,`createdAt`,`userId`) VALUES (?)";
+        const values = [
+            req.body.desc,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id
+        ]
+        db.query(q, [values], (err, data) => {
+            if (err) return res.status(500).json(err)
+            return res.status(200).json("Post has been created.")
+        })
+    })
+
+}
+module.exports = { getPosts, addPost }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getPosts } = require("../controllers/post.js");
+const { getPosts, addPost } = require("../controllers/post.js");
 
 const router = express.Router();
 
 router.get("/",getPosts);
+router.post("/",addPost);
 
 
 module.exports = router;
