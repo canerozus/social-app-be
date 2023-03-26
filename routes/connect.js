994b95376247043461cc3eac926d0635bf3ee574
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-03-26 22:38:07 +0300

    make it better

--- routes/connect.js
+++ routes/connect.js
@@ -2,9 +2,14 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "social"
+    host: "eu-cdbr-west-03.cleardb.net",
+    user: "be16deaa660c73",
+    password: "d587c9c3",
+    database: "heroku_dc7f476f00a0393"
 })
 module.exports = { db }
+
+// host: "localhost",
+// user: "root",
+// password: "password",
+// database: "social"
