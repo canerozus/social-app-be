55cef514cea95584898106c3f4a1747879505798
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-18 22:05:32 +0300

    test2

--- connect.js
+++ connect.js
@@ -3,12 +3,20 @@ dotenv.config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "eu-cdbr-west-03.cleardb.net",
-    user: "be16deaa660c73",
-    password: "d587c9c3",
-    database: "heroku_dc7f476f00a0393"
+host: "eu-cdbr-west-03.cleardb.net",
+user: "be16deaa660c73",
+password: "d587c9c3",
+database: "heroku_dc7f476f00a0393"
 });
 
 module.exports = { db };
+// host: "eu-cdbr-west-03.cleardb.net",
+// user: "be16deaa660c73",
+// password: "d587c9c3",
+// database: "heroku_dc7f476f00a0393"
+// host: "localhost",
+// user: "root",
+// password: "password",
+// database: "social"
 
 
