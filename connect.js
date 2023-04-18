aa637fd98afbafb19855c72181f3363038414846
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-18 15:50:23 +0300

    test

--- connect.js
+++ connect.js
@@ -3,11 +3,10 @@ dotenv.config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host:process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: "eu-cdbr-west-03.cleardb.net",
+    user: "be16deaa660c73",
+    password: "d587c9c3",
+    database: "heroku_dc7f476f00a0393"
 });
 
 module.exports = { db };
